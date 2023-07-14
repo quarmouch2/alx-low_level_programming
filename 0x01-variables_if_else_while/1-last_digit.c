e430e6b3b2f81c544fc3a7063d1974134f41164c
quarmouch2 <adamquarmouch2@gmail.com>
2023-07-14 09:15:49 -0700

    betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
  * main - entry point
  *
  * Description: print the value of n status:
- *		greater than, is zero and is not less then 6.
+ *		greater than, is zero and is not less than 6.
  *
  * Return: 0 (success)
 */
