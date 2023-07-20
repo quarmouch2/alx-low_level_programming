3f4ac8fa7521af53b813ac0af2335b4c230a0aae
quarmouch2 <adamquarmouch2@gmail.com>
2023-07-20 08:49:41 -0700

    0x04

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - print 0 - 14 ten times and you 
+ * more_numbers - print 0 - 14 ten times and you
  *              can only use _putchar three times
  *
  * Return: Always 0 (Success)
